117c9353d8beea7c76793918ca6c88060fb9edac
user <kaposc12@inbox.lv>
2020-01-30 13:57:20 +0200

    20200130_13_55

---
+++ p_r_itons/diary_20200130.py
@@ -0,0 +1,53 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> a=10
+>>> 1
+1
+>>> a*a
+100
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+Traceback (most recent call last):
+  File "/home/user/RTR108/python_test.py", line 7, in <module>
+    print(cos(a))
+NameError: name 'cos' is not defined
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+-0.8390715290764524
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+-0.8390715290764524
+Traceback (most recent call last):
+  File "/home/user/RTR108/python_test.py", line 10, in <module>
+    print(cos(a))
+NameError: name 'cos' is not defined
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+-0.8390715290764524
+-0.8390715290764524
+>>> 
+================= RESTART: /home/user/RTR108/python_test.py =================
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_test.py', 'a': 10}
+-0.8390715290764524
+-0.8390715290764524
+(-0.8390715290764524+0j)
+-0.8390715290764524
+>>> 
---
+++ p_r_itons/python_test.py
@@ -0,0 +1,16 @@
+#print vars() #for python2
+print(vars())
+a=10
+print("%s"%(vars()))
+
+import math
+print(math.cos(a))
+
+from math import cos as cos_V1
+print(cos_V1(a))
+
+from cmath import cos as cos_V2
+print(cos_V2(a))
+
+from math import *
+print(cos(a))
